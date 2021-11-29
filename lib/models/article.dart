0abe35c7ed0f231e2eda063eb70786a4323e99a4
dewdew88 <dewicandani8@gmail.com>
2021-11-29 12:28:36 +0800

    Add parseArticles function and update Article.fromJson

--- lib/models/article.dart
+++ lib/models/article.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class Article {
   late String author;
   late String title;
@@ -15,12 +17,23 @@ class Article {
     required this.content,
   });
 
-  Article.fromJson(Map<String, dynamic> article) {
-    author = article['author'];
-    title = article['title'];
-    url = article['url'];
-    urlToImage = article['urlToImage'];
-    publishedAt = article['publishedAt'];
-    content = article['content'];
+  factory Article.fromJson(Map<String, dynamic> article) {
+    return Article(
+        author: article['author'],
+        title: article['title'],
+        url: article['url'],
+        urlToImage: article['urlToImage'],
+        publishedAt: article['publishedAt'],
+        content: article['content'],
+    );
   }
+}
+
+List<Article> parseArticles(String? json) {
+  if (json == null) {
+    return [];
+  }
+
+  final List parsed = jsonDecode(json);
+  return parsed.map((json) => Article.fromJson(json)).toList();
 }
