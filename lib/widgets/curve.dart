480de56feaa554eccebb44851e0a15db6f65e8cc
dewdew88 <dewicandani8@gmail.com>
2021-12-01 14:15:07 +0800

    update the curve

--- lib/widgets/curve.dart
+++ lib/widgets/curve.dart
@@ -3,97 +3,109 @@ import 'package:capstone/data/api_models/vaccination.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
-class Curve extends StatefulWidget {
+class VaccinationCurve extends StatefulWidget {
+  const VaccinationCurve({Key? key}) : super(key: key);
+
   @override
   _CurveState createState() => _CurveState();
 }
 
-class _CurveState extends State<Curve> {
+class _CurveState extends State<VaccinationCurve> {
   @override
   Widget build(BuildContext context) {
-    List<int> values = [];
+    List<int> values1 = [];
+    List<int> values2 = [];
 
-    return Scaffold(
-        body: FutureBuilder(
-            future: ApiService().fetchVaccination(),
-            builder: (context, AsyncSnapshot<Vaccination> snapshot) {
-              if (!snapshot.hasData) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasData) {
-                for (int i = 1; i < snapshot.data!.harian.length; i++) {
-                  values.add(snapshot.data!.harian[i].jumlahKumVaksinasi1.value);
-                }
-                List<FlSpot> spots = values.asMap().entries.map((e) {
-                  return FlSpot(e.key.toDouble(), e.value.toDouble());
-                }).toList();
+    return FutureBuilder(
+        future: ApiService().fetchVaccination(),
+        builder: (context, AsyncSnapshot<Vaccination> snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasData) {
+            for (int i = 0; i < snapshot.data!.harian.length; i++) {
+              values1.add(snapshot.data!.harian[i].jumlahKumVaksinasi1.value);
+              values2.add(snapshot.data!.harian[i].jumlahKumVaksinasi2.value);
+            }
+            List<FlSpot> spots1 = values1.asMap().entries.map((e) {
+              return FlSpot(e.key.toDouble(), e.value.toDouble());
+            }).toList();
+            List<FlSpot> spots2 = values2.asMap().entries.map((e) {
+              return FlSpot(e.key.toDouble(), e.value.toDouble());
+            }).toList();
 
-                return Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadiusDirectional.circular(4),
-                      color: Colors.grey[350],
-                    ),
-                    height: MediaQuery.of(context).size.height / 2,
-                    child: Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: LineChart(
-                        LineChartData(
-                          lineBarsData: [
-                            LineChartBarData(
-                              spots: spots,
-                              isCurved: true,
-                              barWidth: 3,
-                              belowBarData: BarAreaData(
-                                show: true,
-                                colors: [
-                                  Colors.lightBlueAccent.withOpacity(0.5)
-                                ],
-                              ),
-                              dotData: FlDotData(show: false),
-                            ),
-                          ],
-                          titlesData: FlTitlesData(
+            return Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadiusDirectional.circular(4),
+                  color: Colors.grey[350],
+                ),
+                height: MediaQuery.of(context).size.width * 0.95 * 0.75,
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: LineChart(
+                    LineChartData(
+                      lineBarsData: [
+                        LineChartBarData(
+                          spots: spots1,
+                          isCurved: true,
+                          barWidth: 3,
+                          belowBarData: BarAreaData(
                             show: true,
-                            bottomTitles: SideTitles(
-                              showTitles: false,
-                            ),
-                            rightTitles: SideTitles(
-                              showTitles: false,
-                            ),
-                            topTitles: SideTitles(
-                              showTitles: false,
-                            ),
-                            leftTitles: SideTitles(
-                              showTitles: true,
-                            ),
+                            colors: [Colors.lightBlueAccent.withOpacity(0.5)],
                           ),
-                          axisTitleData: FlAxisTitleData(
-                              topTitle: AxisTitle(
-                                showTitle: true,
-                                titleText: 'Vaccination Curve',
-                                textStyle: const TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              )
-                          ),
-                          gridData: FlGridData(
+                          dotData: FlDotData(show: false),
+                        ),
+                        LineChartBarData(
+                            spots: spots2,
+                            isCurved: true,
+                            barWidth: 2,
+                            belowBarData: BarAreaData(
                               show: true,
-                              checkToShowVerticalLine: (double value) {
-                                return value == 1;
-                              }
+                              colors: [Colors.blueAccent.withOpacity(0.7)],
+                            ),
+                            dotData: FlDotData(show: false),
+                            colors: [Colors.blue]),
+                      ],
+                      titlesData: FlTitlesData(
+                        show: true,
+                        bottomTitles: SideTitles(
+                          showTitles: false,
+                        ),
+                        rightTitles: SideTitles(
+                          showTitles: false,
+                        ),
+                        topTitles: SideTitles(
+                          showTitles: false,
+                        ),
+                        leftTitles: SideTitles(
+                          showTitles: true,
+                        ),
+                      ),
+                      axisTitleData: FlAxisTitleData(
+                        topTitle: AxisTitle(
+                          showTitle: true,
+                          titleText: 'Vaccination Curve',
+                          textStyle: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
                       ),
+                      gridData: FlGridData(
+                          show: true,
+                          checkToShowVerticalLine: (double value) {
+                            return value == 1;
+                          }),
                     ),
                   ),
-                );
-              } else {
-                return Text('Error');
-              }
-            }
-        )
+                ),
+              ),
+            );
+          } else {
+            return Text('Error');
+          }
+        }
     );
   }
 }
