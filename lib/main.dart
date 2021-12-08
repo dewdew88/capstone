3dbc727751027289529ba5529a4f156f8e1534fb
dewdew88 <61006747+dewdew88@users.noreply.github.com>
2021-12-08 21:17:59 +0800

    Revert "Revert "Feature/vaccinelocation""

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,27 @@
 import 'package:capstone/data/models/article.dart';
 import 'package:capstone/data/models/vaccine.dart';
 import 'package:capstone/data/preferences/preferences_helper.dart';
+import 'package:capstone/provider/history_provider.dart';
 import 'package:capstone/provider/preferences_provider.dart';
 import 'package:capstone/screens/article_detail_page.dart';
 import 'package:capstone/screens/article_page.dart';
 import 'package:capstone/screens/main_page.dart';
 import 'package:capstone/screens/on_boarding_page.dart';
 import 'package:capstone/screens/type_vaccine_details_page.dart';
+import 'package:capstone/screens/vaccination_registration_page.dart';
 import 'package:capstone/screens/vaccine_detail_page.dart';
 import 'package:capstone/screens/vaccines_page.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:capstone/widgets/article_web_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'data/models/type_vaccine.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -33,6 +38,9 @@ class MyApp extends StatelessWidget {
               sharedPreferences: SharedPreferences.getInstance(),
             ),
           ),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => HistoryProvider(),
         ),
       ],
       child: Consumer<PreferencesProvider>(
@@ -60,11 +68,14 @@ class MyApp extends StatelessWidget {
               TypeVaccineDetails.routeName: (context) => TypeVaccineDetails(
                   vaccine: ModalRoute.of(context)?.settings.arguments
                       as TypeVaccine),
-              VaccinePage.routeName: (context) => VaccinePage(),
+              VaccinePage.routeName: (context) => const VaccinePage(),
               VaccineDetailPage.routeName: (context) => VaccineDetailPage(
                     vaccineLoc:
                         ModalRoute.of(context)?.settings.arguments as Vaccine,
-                  )
+                  ),
+              Registration.routeName: (context) => Registration(
+                klinik: ModalRoute.of(context)?.settings.arguments as String,
+              ),
             },
           );
         },
